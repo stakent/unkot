a77f2d3bfca72d27b2affa3fc9e5fc3fdadf2515
Dariusz Walat <>
2022-01-26 11:34:46 +0100

    connect to postgres using tcp to be consistent: dev, staging, production

--- env-local.sh
+++ env-local.sh
@@ -1,4 +1,5 @@
-export DATABASE_URL=postgres:///unkot_dev
+# DATABASE_URL="postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}
+export DATABASE_URL=postgres://unkot_dev:XuYuYyJWhtL2rV1pSJ3sA@127.0.0.1:5432/unkot_dev
 export USE_DOCKER=no
 export CELERY_BROKER_URL=redis://localhost:6379/0
 
