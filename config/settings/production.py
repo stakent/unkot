4a9fc609cbed4530bcff9fbca4df22235b8fb203
Dariusz Walat <>
2022-02-08 09:31:19 +0100

    mailjet email backend configured for production

--- config/settings/production.py
+++ config/settings/production.py
@@ -87,11 +87,11 @@ INSTALLED_APPS += ["anymail"]  # noqa F405
 # https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
 # https://anymail.readthedocs.io/en/stable/installation/#anymail-settings-reference
 # https://anymail.readthedocs.io/en/stable/esps/mailgun/
-EMAIL_BACKEND = "anymail.backends.mailgun.EmailBackend"
+EMAIL_BACKEND = "anymail.backends.mailjet.EmailBackend"
 ANYMAIL = {
-    "MAILGUN_API_KEY": env("MAILGUN_API_KEY"),
-    "MAILGUN_SENDER_DOMAIN": env("MAILGUN_DOMAIN"),
-    "MAILGUN_API_URL": env("MAILGUN_API_URL", default="https://api.mailgun.net/v3"),
+    "MAILJET_API_KEY": "6afa41b16b7f3b3e8257e724d6133400",
+    "MAILJET_SECRET_KEY": "8aa706e5c270a019b0463dda1fe76c66",
+    "MAILJET_API_URL": "https://api.mailjet.com/v3.1/",
 }
 
 
