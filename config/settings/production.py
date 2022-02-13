b392e62c6d9c3321dfb9856fdd670252e2a268d2
Dariusz Walat <>
2022-02-13 18:00:07 +0100

    unkot FROM email changed

--- config/settings/production.py
+++ config/settings/production.py
@@ -65,14 +65,14 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-from-email
 DEFAULT_FROM_EMAIL = env(
     "DJANGO_DEFAULT_FROM_EMAIL",
-    default="unkot <noreply@unkot.pl>",
+    default="kontakt <kontakt@unkot.pl>",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#server-email
 SERVER_EMAIL = env("DJANGO_SERVER_EMAIL", default=DEFAULT_FROM_EMAIL)
 # https://docs.djangoproject.com/en/dev/ref/settings/#email-subject-prefix
 EMAIL_SUBJECT_PREFIX = env(
     "DJANGO_EMAIL_SUBJECT_PREFIX",
-    default="[unkot]",
+    default="[unkot.pl]",
 )
 
 # ADMIN
