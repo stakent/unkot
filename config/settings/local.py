127340f25d1e8e7949d6803c5384aa3d088ddb04
Dariusz Walat <>
2022-02-14 09:46:49 +0100

    run tasks using celery

--- config/settings/local.py
+++ config/settings/local.py
@@ -74,6 +74,6 @@ INSTALLED_APPS += ["django_extensions"]  # noqa F405
 # ------------------------------------------------------------------------------
 
 # http://docs.celeryproject.org/en/latest/userguide/configuration.html#task-eager-propagates
-CELERY_TASK_EAGER_PROPAGATES = True
+CELERY_TASK_EAGER_PROPAGATES = False
 # Your stuff...
 # ------------------------------------------------------------------------------
