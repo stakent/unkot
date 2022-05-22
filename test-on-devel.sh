fb190ec587f93a053c49edf1a128af718ba2f763
Dariusz Walat <>
2022-05-22 18:14:01 +0200

    run cypress tests on devel against django server with DEBUG=False

--- test-on-devel.sh
+++ test-on-devel.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 
 set -e
+set -m
 
 # source: https://misc.flogisoft.com/bash/tip_colors_and_formatting
 GREEN='\e[32m'
 RED='\e[31m'
 
-DEV_URL='http://127.0.0.1:8001'
+PORT=8002
+DEV_URL="http://127.0.0.1:${PORT}"
+
+# run django development server with DEBUG=False
+DJANGO_READ_DOT_ENV_FILE=True DJANGO_DEBUG=False ./manage.py runserver 0.0.0.0:${PORT} &
+
+
+# make sure MailHog receiving emails from django app is runnig
+if [  `ps a | grep MailHog | wc -l` == 1 ]; then
+	./MailHog_linux_amd64 &
+fi
 
 # django tests 
 dropdb --if-exists test_unkot_dev
 ./manage.py test --no-input --keepdb
 
-# cypress tests against devel server
 cp .envs/cypress/devel.json cypress.env.json
-./node_modules/.bin/cypress run
+CYPRESS_baseUrl=${DEV_URL} ./node_modules/.bin/cypress run
 
 echo -e "${GREEN}Done testing on devel"
